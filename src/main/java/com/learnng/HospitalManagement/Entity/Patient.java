8e569768b5fecd71d2bc59fda0d4796db708306b
Anurag Sen <anurag007sen@gmail.com>
2026-05-13 00:03:52 +0530

    patinet enety added

---
+++ src/main/java/com/learnng/HospitalManagement/Entity/Patient.java
@@ -0,0 +1,21 @@
+package com.learnng.HospitalManagement.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private LocalDate birthDate;
+    private String emai
+    private String gender;
+}
